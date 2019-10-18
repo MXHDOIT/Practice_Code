383c5bb000b9261585422f1d0cee0722c400d4f8
MXHDOIT <2826372596@qq.com>
2019-10-18 21:32:37 +0800

    update

--- JAVA/day02/Fibola.java
+++ JAVA/day02/Fibola.java
@@ -8,4 +8,21 @@ public class Fibola {
             return 1;
         return f(n-1)+f(n-2);
     }
+
+   public static int fbola2(int n){
+        int f1 = 1;
+        int f2 = 1;
+        if(n == 1||n == 2){
+            return 1;
+        }
+        int f3 = 0;
+        for(int i = 3;i <= n;i++){
+            f3 = f1+f2;
+            f1 = f2;
+            f2 = f3;
+        }
+
+        return f3;
+
+    }
 }
--- JAVA/day02/PrintNum.java
+++ JAVA/day02/PrintNum.java
@@ -1,17 +1,21 @@
 public class PrintNum {
     public static void main(String[] args){
+
         print(1234);
     }
 
     public static void print(int num){
-        if(num < 10)
-        {
-            System.out.print(num);
-        }else {
+//        if(num < 10)
+//        {
+//            System.out.print(num);
+//        }else {
+//            print(num/10);
+//            System.out.print(num%10);
+//        }
+
+        if(num > 9) {
             print(num/10);
-            System.out.print(num%10);
         }
-
-
+        System.out.println(num%10);
     }
 }

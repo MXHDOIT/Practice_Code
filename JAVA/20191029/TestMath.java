881236ce423ec6f33016c330155a0131b11db7e1
MXHDOIT <2826372596@qq.com>
2019-10-30 09:31:27 +0800

    20191030补

---
+++ JAVA/20191029/TestMath.java
@@ -0,0 +1,15 @@
+public class TestMath {
+    public static void main(String[] args) {
+        double min = -10.8;
+        double max = 5.9;
+
+        int count = 0;
+
+        for(double i = Math.ceil(min);i <= max;i++){
+            if(Math.abs(i) > 6|| Math.abs(i)<2.1){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}

93cbfb703238ad97ed46470bf46ed55da63a2ab8
MXHDOIT <2826372596@qq.com>
2019-10-31 22:49:28 +0800

    20191031

--- JAVA/20191031USB接口/Keyboard.java
+++ JAVA/20191031USB接口/Keyboard.java
@@ -1,18 +1,15 @@
-package cn.itcast.day10.demo07;
-
-// 键盘就是一个USB设备
-public class Keyboard implements USB {
+public class KeyBoard implements USB {
     @Override
     public void open() {
-        System.out.println("打开键盘");
+        System.out.println("键盘开启，绿灯闪一闪");
     }
 
     @Override
     public void close() {
-        System.out.println("关闭键盘");
+        System.out.println("键盘关闭，绿灯熄灭");
     }
 
-    public void type() {
-        System.out.println("键盘输入");
+    public void type(){
+        System.out.println("键盘打字");
     }
 }

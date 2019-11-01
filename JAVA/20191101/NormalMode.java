0f0903484567514814bb7192ae8cb4f72568ba6d
MXHDOIT <2826372596@qq.com>
2019-11-01 22:27:40 +0800

    20191101

---
+++ JAVA/20191101/NormalMode.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class NormalMode implements OpenMode {
+    @Override
+    public ArrayList<Integer> divide(int totalMoney, int totalCount) {
+        ArrayList<Integer>  list = new ArrayList<>();
+
+        int avg = totalMoney / totalCount;
+        int mod = totalCount % totalCount;
+
+        for (int i = 0; i < totalCount - 1; i++) {
+            list.add(avg);
+        }
+
+        list.add(avg+mod);
+        return list;
+    }
+}

82b70b405694812d075d9563c6e31c31a3c4451b
MXHDOIT <2826372596@qq.com>
2019-11-01 22:26:03 +0800

    20191101

---
+++ JAVA/20191101/Bootstrap.java
@@ -0,0 +1,7 @@
+public class Bootstrap {
+    public static void main(String[] args) {
+        MyRed red = new MyRed("413");
+        red.setOwnerName("马新航");
+        red.setOpenWay(new NormalMode());
+    }
+}

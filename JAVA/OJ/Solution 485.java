8d91667c5610269b28328de357f69064dd45aaaa
MXHDOIT <2826372596@qq.com>
2019-11-12 22:44:24 +0800

    20191112

---
+++ JAVA/OJ/Solution 485.java
@@ -0,0 +1,23 @@
+//最大连续1的个数
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int max = 0;
+        
+        for(int i = 0;i < nums.length;i++){
+            if(nums[i] == 1){
+                count++;
+            }
+            
+            if(nums[i] == 0){
+                count = 0;
+            }
+            
+            if(count > max){
+                max= count;
+            }
+        }
+        
+        return max;
+    }
+}

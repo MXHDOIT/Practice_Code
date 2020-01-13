f858ca1f4135ce2cb56f9d678e8358e98325f38d
MXHDOIT <2826372596@qq.com>
2020-01-13 23:25:14 +0800

    20200113

---
+++ JAVA/OJ/Soultion383.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        StringBuilder stringBuilder = new StringBuilder(magazine);
+
+        int index;
+        for(char c:ransomNote.toCharArray()){
+            index = stringBuilder.indexOf(String.valueOf(c));
+            if(index >= 0){
+                stringBuilder.deleteCharAt(index);
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}

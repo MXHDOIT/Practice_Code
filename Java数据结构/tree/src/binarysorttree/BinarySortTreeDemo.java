afee605ec50fc1925894506bc145fb57c057b955
MXHDOIT <2826372596@qq.com>
2020-02-13 00:05:10 +0800

    20200213

--- Java数据结构/tree/src/binarysorttree/BinarySortTreeDemo.java
+++ Java数据结构/tree/src/binarysorttree/BinarySortTreeDemo.java
@@ -2,10 +2,38 @@ package binarysorttree;
 
 public class BinarySortTreeDemo {
     public static void main(String[] args) {
+        int[] arr = {7,3,10,12,5,1,9};
+        BinarySortTree binarySortTree = new BinarySortTree();
+        for(int i = 0;i < arr.length;i++){
+            binarySortTree.add(new Node(arr[i]));
+        }
 
+        binarySortTree.infixOrder();
     }
 }
 
+
+//创建二叉树
+class BinarySortTree{
+    private Node root;
+    //添加节点
+    public void add(Node node){
+        if(root == null){
+            root = node;
+        }else{
+            root.add(node);
+        }
+    }
+    //中序遍历
+    public void infixOrder(){
+        if(root != null){
+            root.infixOrder();
+        }else{
+            System.out.println("二叉排序树为空，不能遍历");
+        }
+    }
+}
+//创建节点
 class Node{
     int value;
     Node left;
@@ -21,5 +49,72 @@ class Node{
         if(node == null){
             return ;
         }
+
+        //判断传入的值，和当前节点的关系
+        if(this.value > node.value){
+            if(this.left == null){
+                this.left = node;
+            }else{
+                this.left.add(node);
+            }
+        }else{
+            if(this.right == null){
+                this.right = node;
+            }else{
+                this.right.add(node);
+            }
+        }
+    }
+
+    //中序遍历
+    public void infixOrder(){
+        if(this.left!=null){
+            this.left.infixOrder();
+        }
+        System.out.println(this);
+        if(this.right!=null){
+            this.right.infixOrder();
+        }
+    }
+
+    //查找要删除的节点
+    public Node search(int value){
+        if(value == this.value){ //找到的就是节点
+            return this;
+        }else if(value < this.value){
+            if(this.left == null){
+                return null;
+            }
+            return this.left.search(value);
+        }else{
+            if(this.right == null){
+                return null;
+            }
+            return this.right.search(value);
+        }
+
+
+    }
+
+    //查找要删除节点的父节点
+    public Node searchParent(int value){
+        if((this.left != null && this.left.value == value)||(this.right != null&&this.right.value == value)){
+            return this;
+        }else{
+            //如果查找的值小于当前节点的值，并且当前节点的左子结点不为空
+            if(value < this.value && this.left != null){
+                return this.left.searchParent(value);
+            }else if(value >= this.value && this.right != null){
+                return this.right.searchParent(value);
+            }else{
+                return null;
+            }
+        }
+    }
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                '}';
     }
 }

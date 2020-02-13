afb52545c280aa1c80f57085a6e849586f8ee5e7
MXHDOIT <2826372596@qq.com>
2020-02-13 15:29:25 +0800

    20200213

--- Java数据结构/tree/src/binarysorttree/BinarySortTreeDemo.java
+++ Java数据结构/tree/src/binarysorttree/BinarySortTreeDemo.java
@@ -2,12 +2,16 @@ package binarysorttree;
 
 public class BinarySortTreeDemo {
     public static void main(String[] args) {
-        int[] arr = {7,3,10,12,5,1,9};
+        int[] arr = {7,3,10,12,5,1,9,2};
         BinarySortTree binarySortTree = new BinarySortTree();
         for(int i = 0;i < arr.length;i++){
             binarySortTree.add(new Node(arr[i]));
         }
 
+////        binarySortTree.infixOrder();
+//        binarySortTree.deleteNode(5);
+//        binarySortTree.deleteNode(2);
+        binarySortTree.deleteNode(7);
         binarySortTree.infixOrder();
     }
 }
@@ -30,6 +34,89 @@ class BinarySortTree{
             root.infixOrder();
         }else{
             System.out.println("二叉排序树为空，不能遍历");
+        }
+    }
+
+    //查找要删除的节点
+    public Node search(int value){
+        if(root== null){
+            return null;
+        }else {
+            return root.search(value);
+        }
+    }
+    //查找要删除节点的父节点
+    public Node searchParent(int value){
+        if(root== null){
+            return null;
+        }else {
+            return root.searchParent(value);
+        }
+    }
+
+    //删除最小数
+     public int delRightMin(Node node){
+        Node cur = node.right;
+
+        while(cur.left != null){
+            cur = cur.left;
+        }
+
+        deleteNode(cur.value);
+        return cur.value;
+     }
+
+    //删除节点
+    public void deleteNode(int value){
+        if(root == null){
+            return ;
+        }else{
+            //查找要删除的节点
+            Node curNode = search(value);
+            //判断是否找到
+            if(curNode == null){
+                return ;
+            }
+            //判断根节点
+            if(root.right == null && root.left == null){
+                root = null;
+                return ;
+            }
+            //获取父节点
+            Node parentNode = searchParent(value);
+            //删除
+            if(curNode.left == null && curNode.right == null){
+                if(parentNode.left!=null&&parentNode.left.value == value){
+                    parentNode.left = null;
+                    return ;
+                }else if(parentNode.right!=null&&parentNode.right.value == value){
+                    parentNode.right = null;
+                    return ;
+                }
+            }else if(curNode.right != null&& curNode.left != null){
+                curNode.value = delRightMin(curNode);
+            }else{
+                if(parentNode == null){
+                    if(curNode.left != null){
+                        root = curNode.left;
+                    }else if(curNode.right != null){
+                        root = curNode.right;
+                    }
+                }
+                if(parentNode.left == curNode){
+                    if(curNode.left != null){
+                        parentNode.left = curNode.left;
+                    }else if(curNode.right != null){
+                        parentNode.left = curNode.right;
+                    }
+                }else if(parentNode.right == curNode){
+                    if(curNode.left != null){
+                        parentNode.right = curNode.left;
+                    }else if(curNode.right != null){
+                        parentNode.right = curNode.right;
+                    }
+                }
+            }
         }
     }
 }

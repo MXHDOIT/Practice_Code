081f2429cff088535ea3bdb669615e1f944ddde5
MXHDOIT <2826372596@qq.com>
2020-03-07 09:48:00 +0800

    20200307

--- Java数据结构/tree/src/Solution.java
+++ Java数据结构/tree/src/Solution.java
@@ -1,43 +1,37 @@
 import java.util.ArrayList;
+import java.util.List;
 
-public class Solution {
-    public static void main(String[] args) {
-        int[][] array = {
-                {1,2,3,4},
-                {5,6,7,8},
-                {9,10,11,12},
-                {13,14,15,16}
-        };
+class Solution {
+    public static int[][] findContinuousSequence(int target) {
+        List<List<Integer>> res = new ArrayList<>();
 
-        System.out.println(printMatrix(array));
+        for(int i = 1;i <= target/2;i++){
+            int count = 0;
+            List<Integer> list = new ArrayList<>();
+            for(int k = i;count < target;k++){
+                count += k;
+                list.add(k);
+            }
+            if(count == target){
+                res.add(list);
+            }
+        }
+
+        int[][] resArr = new int[res.size()][];
+        for(int i = 0;i < res.size();i++){
+            resArr[i] = new int[res.get(i).size()];
+        }
+
+        for(int i = 0;i < resArr.length;i++){
+            for(int j = 0;j < resArr[i].length;j++){
+                resArr[i][j] = res.get(i).get(j);
+            }
+        }
+
+        return resArr;
     }
 
-    public static ArrayList<Integer> printMatrix(int [][] array) {
-        ArrayList<Integer> result = new ArrayList<Integer> ();
-        if(array.length < 1){
-            return result;
-        }
-        int down = array.length;
-        int right = array[0].length;
-        if(right < 1){
-            return result;
-        }
-
-        int layer = (Math.min(right,down)-1)/2+1;
-        for(int i = 0;i < layer;i++){
-            for(int c = i;c < right-i;c++){
-                result.add(array[i][c]);
-            }
-            for(int r = i+1;r < down-i;r++){
-                result.add(array[r][right-i-1]);
-            }
-            for(int c = right-2-i;(c>=i)&&(down-i-1 != i);c--){ //down-i-1 != i判断是否在一行
-                result.add(array[down-i-1][c]);
-            }
-            for(int r = down-2-i; (r>i)&&(right-i-1 != i);r--){
-                result.add(array[r][i]);
-            }
-        }
-        return result;
+    public static void main(String[] args) {
+        findContinuousSequence(9);
     }
 }

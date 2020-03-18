cb24d252efaaf95a47a57d095ea1c578ffd21683
MXHDOIT <2826372596@qq.com>
2020-03-18 23:46:21 +0800

    20200318

--- JAVA/lanqiao/src/lanqiao/Main_4.java
+++ JAVA/lanqiao/src/lanqiao/Main_4.java
@@ -5,23 +5,37 @@ public class Main_4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner in = new Scanner(System.in);
-		String str1 = in.next();
+//		Scanner in = new Scanner(System.in);
+//		String str1 = in.next();
+//		
+//		String str2 = in.next();
+//		int index =0;
+//		if(str1.length() != str2.length()) {
+//			index = 1;
+//		}else if(str1.equals(str2)) {
+//			index = 2;
+//		}else if(str1.equalsIgnoreCase(str2)) {
+//			index = 3;
+//		}else {
+//			index = 4;
+//		}
+//		
+//		
+//		System.out.print(index);
 		
-		String str2 = in.next();
-		int index =0;
-		if(str1.length() != str2.length()) {
-			index = 1;
-		}else if(str1.equals(str2)) {
-			index = 2;
-		}else if(str1.equalsIgnoreCase(str2)) {
-			index = 3;
-		}else {
-			index = 4;
-		}
+		int i= 0;
 		
+		loop :
+			 for(int j = 0;j <10;j++) {
+				 for(int k = 9;k > 0;k--) {
+					 i++;
+					 if(i==3) {
+						 break loop;
+					 }
+				 }
+			 }
 		
-		System.out.print(index);
+		System.out.print(i);
 	}
 
 }
